4839a8f633105a483d38f428f6f053279350e9cd
Vincent <vinz2007@hotmail.com>
2017-06-07 08:55:07 +0200

    image wasn't working

--- js/setup.js
+++ js/setup.js
@@ -92,14 +92,16 @@ function initialiseAll(isoCode) {
 
  
 	if(isoCode=="PIC"){
-
 		d3.select("#viz")
 	      .append('svg')
 	      .attr('width', 900)
 	      .attr('height', 430)
 		.append("svg:image")
-		      .attr("xlink:href", "pics/intro.jpg")
-		      .attr("width", width);
+		   .attr('x',0)
+		   .attr('y',0)
+		   .attr('width', 900)
+		   .attr('height', 430)
+		      .attr("xlink:href", "pics/intro.jpg");
 
 	}else{
 		// Load the data.
